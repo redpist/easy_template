3a3fe800d6cfd631ec4a72450ce7fafbe25d6549
Jeremy Lecerf <redpist@redpist.com>
2016-05-30 10:51:20 +0200

    Regexp escape variable

--- lib/easy_template.rb
+++ lib/easy_template.rb
@@ -5,6 +5,6 @@ module EasyTemplate
   #   variables: (Hash)
   #         the hash containing the values to substitute in place of the keys enclosed by { and }.
   def self.process(text, variables)
-    text.gsub(/(\\\{)|((?<!\\)(?:\\\\)*#{variables.map{|v|"\\{#{v[0]}\\}"}.join('|')})/i){|s| s[0] == '\\' ? s[1] : variables[s[1..-2]]}
+    text.gsub(/(\\\{)|((?<!\\)(?:\\\\)*#{variables.map{|v|"\\{#{Regexp.escape(v[0])}\\}"}.join('|')})/i){|s| s[0] == '\\' ? s[1] : variables[s[1..-2]]}
   end
 end
